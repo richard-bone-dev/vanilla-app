ea7558cba1cb1806408d95ea251e2204c3cfc98f
boner28 <bone.r28@hotmail.com>
2026-04-28 03:30:13 +0100

    API  working

--- api/src/Vanilla.Api/Program.cs
+++ api/src/Vanilla.Api/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using Vanilla.Api;
 using Vanilla.Application;
 using Vanilla.Infrastructure;
@@ -12,10 +13,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSingleton<IClock, SystemClock>();
 builder.Services.AddScoped<LedgerApplicationService>();
 builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
+builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
 await DatabaseInitializer.InitializeAsync(app.Services);
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/debug/environment", (IHostEnvironment env) => new
+    {
+        env.EnvironmentName,
+        env.ApplicationName,
+        env.ContentRootPath
+    });
+}
+
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.MapLedgerApi();
 
